f07eaffb3e8c890294e1c0c53f44b97c1c0604ef
Nandana <115874551+NanduNanduz@users.noreply.github.com>
2023-10-09 00:21:48 +0530

    Login Form updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import { ChatEngine } from 'react-chat-engine';
+import LoginForm from './components/LoginForm';
 import './App.css';
 import  ChatFeed  from './components/ChatFeed' ;
 
 const App = () => {
-    return (
-      <ChatEngine
-        height="100vh"
-        projectID="182f9df3-5958-4db9-9c18-56e6df21345c"
-        userName="Zera"
-        userSecret="12345678"
-        renderChatFeed={(chatAppProps) =><ChatFeed {...chatAppProps}
-        />}
-      />
-    );
+  //not loggin in
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="182f9df3-5958-4db9-9c18-56e6df21345c"
+      userName="Zera"
+      userSecret="12345678"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+  );
 };
 
 export default App;
